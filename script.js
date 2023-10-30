779d11034f4f2990520dc2e4edb94122f6ff4b59
THALES SOARES DE ARAÚJO <thales_araujo@estudante.sesisenai.org.br>
2023-10-30 20:28:54 -0300

    Versão 1.0

---
+++ script.js
@@ -0,0 +1,42 @@
+
+function mudarTudo(){
+    bg = document.querySelector(".bg");
+    container = document.querySelector(".container");
+    nav = document.querySelector("nav");
+
+    bg.classList.toggle("troll1");
+    container.classList.toggle("troll2");
+    nav.classList.toggle("troll3");
+}
+
+function scrollFront(){
+    window.scrollTo(0, 0);
+}
+
+function scrollDesign(){
+    window.scrollTo(0,400);
+}
+
+function scrollCores(){
+    window.scrollTo(0, 700);
+}
+
+function scrollResponsividade(){
+    window.scrollTo(0, 850);
+}
+
+function scrollUsabilidade(){
+    window.scrollTo(0, 1100);
+}
+
+function scrollAcessibilidade(){
+    window.scrollTo(0, 1200);
+}
+
+function scrollInteratividade(){
+    window.scrollTo(0, 1200);
+}
+
+function scrollFeedback(){
+    window.scrollTo(0, 1200);
+}
